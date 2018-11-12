49fefa610344eece41526efecb9fa8594806ab58
Konstantin Kostin <newuser9929@gmail.com>
2018-11-12 21:04:08 +0300

    Add service at 'get more' button

--- XkcdBrowser/app/src/main/java/com/learning/newuserkk/xkcdbrowser/ImagesListActivity.kt
+++ XkcdBrowser/app/src/main/java/com/learning/newuserkk/xkcdbrowser/ImagesListActivity.kt
@@ -1,19 +1,25 @@
 package com.learning.newuserkk.xkcdbrowser
 
 import android.Manifest.permission.INTERNET
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
 import android.content.pm.PackageManager
+import android.os.Binder
 import android.os.Build
 import android.os.Bundle
+import android.os.IBinder
 import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.RecyclerView
 import android.util.Log
-import com.learning.newuserkk.xkcdbrowser.picture.FetchComicAsyncTask
-import com.learning.newuserkk.xkcdbrowser.picture.FetchAllComicsAsyncTask
-import com.learning.newuserkk.xkcdbrowser.picture.PictureFetcher
+import android.widget.Toast
+import com.learning.newuserkk.xkcdbrowser.picture.*
 import kotlinx.android.synthetic.main.activity_images_list.*
 
 import kotlinx.android.synthetic.main.images_list.*
+import java.net.URL
 
 
 class ImagesListActivity : AppCompatActivity() {
@@ -21,8 +27,6 @@ class ImagesListActivity : AppCompatActivity() {
     companion object {
         const val LOG_TAG = "ImagesListActivity"
         const val COMICS_TO_ADD = 30
-        const val LOADED_COMICS = "LOADED_COMICS"
-        const val SHARED_PREFS_NAME = "ImagesListActivity"
     }
 
     /**
@@ -30,7 +34,10 @@ class ImagesListActivity : AppCompatActivity() {
      * device.
      */
     private var twoPane = false
+
     private lateinit var adapter: PictureRecyclerViewAdapter
+    private lateinit var binder: FetchComicService.FetchComicServiceBinder
+    private lateinit var serviceConnection: ServiceConnection
     private var loadedComicsCount = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -50,7 +57,30 @@ class ImagesListActivity : AppCompatActivity() {
 
         setupRecyclerView(images_list)
 
-        // either saved when orientation changed or activity destroyed
+        serviceConnection = object: ServiceConnection {
+            override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+                Log.d(LOG_TAG, "Service $name connected")
+                binder = service as FetchComicService.FetchComicServiceBinder
+
+                binder.setCallback(object: FetchComicService.LoadCallback {
+                    override fun onLoad(item: XkcdComic?) {
+                        Log.d(LOG_TAG, "Got $item")
+                        item?.let {
+                            Content.addItem(it)
+                            adapter.notifyDataSetChanged()
+                        } ?: Toast.makeText(this@ImagesListActivity,
+                                "Could'n load some comics",
+                                Toast.LENGTH_SHORT)
+                                .show()
+                    }
+                })
+            }
+
+            override fun onServiceDisconnected(name: ComponentName?) {
+                // pass
+            }
+        }
+
         loadedComicsCount = Content.ITEMS.size
 
         if (loadedComicsCount != 0) {
@@ -64,10 +94,19 @@ class ImagesListActivity : AppCompatActivity() {
         addComicButton.setOnClickListener {
             val oldestComicId = Content.getOldestComic()?.id ?: return@setOnClickListener
             for (i in 1..COMICS_TO_ADD) {
-                FetchComicAsyncTask(adapter).execute(
-                        Content.getComicUrl(oldestComicId - i))
+                val comic = Content.getComicUrl(oldestComicId - i) ?: break
+                FetchComicService.startService(this, comic)
             }
+            bindService(Intent(this, FetchComicService::class.java),
+                    serviceConnection,
+                    Context.BIND_AUTO_CREATE)
         }
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unbindService(serviceConnection)
     }
 
     fun fetchStartComics() {
@@ -93,7 +132,7 @@ class ImagesListActivity : AppCompatActivity() {
     }
 
     private fun fetchAllComics() {
-        if (loadedComicsCount == 0) {
+        if (loadedComicsCount < Content.START_COUNT) {
             val task = FetchAllComicsAsyncTask(adapter, this)
             task.execute(Content.getComicUrl())
         }
@@ -104,5 +143,4 @@ class ImagesListActivity : AppCompatActivity() {
                 this, Content.ITEMS, twoPane)
         recyclerView.adapter = adapter
     }
-
 }
