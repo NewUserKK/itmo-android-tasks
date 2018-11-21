e4e500fcb3742532584bddc5ca49c601b9f7b25a
Konstantin Kostin <newuser9929@gmail.com>
2018-11-21 11:34:19 +0300

    Change image load to Picasso instead of service

--- XkcdBrowser/app/src/main/java/com/learning/newuserkk/xkcdbrowser/ImagesDetailFragment.kt
+++ XkcdBrowser/app/src/main/java/com/learning/newuserkk/xkcdbrowser/ImagesDetailFragment.kt
@@ -1,12 +1,6 @@
 package com.learning.newuserkk.xkcdbrowser
 
-import android.content.ComponentName
-import android.content.Context
-import android.content.Intent
-import android.content.ServiceConnection
-import android.graphics.Bitmap
 import android.os.Bundle
-import android.os.IBinder
 import android.support.v4.app.Fragment
 import android.support.v7.app.AlertDialog
 import android.util.Log
@@ -15,12 +9,11 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import com.learning.newuserkk.xkcdbrowser.picture.XkcdComic
-import com.learning.newuserkk.xkcdbrowser.picture.service.DownloadBitmapService
-import com.learning.newuserkk.xkcdbrowser.picture.service.LoadCallback
-import com.learning.newuserkk.xkcdbrowser.picture.service.ServiceBinder
+import com.squareup.picasso.Callback
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.comic_details.*
+import kotlinx.android.synthetic.main.comic_details.view.*
 import java.io.IOException
-import java.lang.IllegalStateException
 
 
 /**
@@ -31,8 +24,8 @@ import java.lang.IllegalStateException
  */
 class ImagesDetailFragment : Fragment() {
 
-    inner class BitmapLoadCallback: LoadCallback<Bitmap> {
-        override fun onLoad(item: Bitmap) {
+    inner class BitmapLoadCallback: Callback {
+        override fun onSuccess() {
             val comic = comic
             if (comic == null) {
                 Log.e(LOG_TAG, "Comic is null!")
@@ -40,11 +33,9 @@ class ImagesDetailFragment : Fragment() {
             }
 
             Log.d(ImagesListActivity.LOG_TAG, "Loaded picture of #${comic.id}")
-            val imageView = detailsComicPicture
-            imageView.setImageBitmap(item)
-            imageView.contentDescription =
+            detailsComicPicture.contentDescription =
                     resources.getString(R.string.detailsComicDescription, comic.alt)
-            detailsComicDescription.text = imageView.contentDescription
+            detailsComicDescription.text = detailsComicDescription.contentDescription
             detailsComicDate.text = resources.getString(
                     R.string.detailsComicDate,
                     comic.day,
@@ -52,15 +43,12 @@ class ImagesDetailFragment : Fragment() {
                     comic.year)
         }
 
-        override fun onException(errors: List<Exception>) {
+        override fun onError(e: java.lang.Exception?) {
             var message = ""
-            for (e in errors) {
-                when (e) {
-                    is IOException -> message += getString(R.string.loadPictureErrorMessage)
-                    is SecurityException -> message += getString(R.string.savePictureErrorMessage)
-                    is IllegalStateException -> message += getString(R.string.decodePictureErrorMessage)
-                }
-                message += "\n"
+            message += when (e) {
+                is IOException -> getString(R.string.loadPictureErrorMessage)
+                is SecurityException -> getString(R.string.savePictureErrorMessage)
+                else -> getString(R.string.unknownPictureErrorMessage)
             }
             AlertDialog.Builder(this@ImagesDetailFragment.context!!)
                     .setMessage(message)
@@ -77,13 +65,9 @@ class ImagesDetailFragment : Fragment() {
     }
 
     private var comic: XkcdComic? = null
-    private lateinit var serviceConnection: ServiceConnection
-    private var binder: ServiceBinder<Bitmap>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        setupServiceConnection()
 
         arguments?.let {
             if (it.containsKey(ARG_ITEM_ID)) {
@@ -93,67 +77,56 @@ class ImagesDetailFragment : Fragment() {
         }
     }
 
-    private fun setupServiceConnection() {
-        serviceConnection = object : ServiceConnection {
-            override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
-                binder = service as ServiceBinder<Bitmap>
-                binder?.setCallback(BitmapLoadCallback())
-            }
-
-            override fun onServiceDisconnected(name: ComponentName?) {
-                binder = null
-            }
-        }
-    }
-
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val rootView = inflater.inflate(R.layout.comic_details, container, false)
 
-        comic?.let { comic ->
-            context?.let {context ->
-                val path = context.cacheDir?.absolutePath + "/${comic.id}.png"
-                DownloadBitmapService.startService(rootView.context, comic, path)
-                context.bindService(Intent(context, DownloadBitmapService::class.java),
-                        serviceConnection,
-                        Context.BIND_AUTO_CREATE)
+        with (rootView) {
+            comic?.let { comic ->
+                context?.let {context ->
+                    Picasso.get().isLoggingEnabled = true
+                    Picasso.get()
+                            .load(comic.imgLink)
+                            .tag(LOG_TAG)
+                            .placeholder(R.drawable.ic_launcher_background)
+                            .into(detailsComicPicture, BitmapLoadCallback())
 
-                container?.setOnTouchListener(object : OnSwipeTouchListener(rootView.context) {
-                    override fun onSwipeRight() {
-                        Log.d(LOG_TAG, "Detected right swipe")
-                        if (!comic.isOldest) {
-                            // TODO: add proper load
-                            if (comic.id == Content.oldestLoadedComic?.id) {
+                    container?.setOnTouchListener(object : OnSwipeTouchListener(rootView.context) {
+                        override fun onSwipeRight() {
+                            Log.d(LOG_TAG, "Detected right swipe")
+                            if (!comic.isOldest) {
+                                // TODO: add proper load
+                                if (comic.id == Content.oldestLoadedComic?.id) {
 //                                Loader.load(context, comic.id - 1)
-                                Toast.makeText(context, getString(R.string.debug_oldestLoadedComicReachedOnSwipe), Toast.LENGTH_SHORT).show()
+                                    Toast.makeText(context, getString(R.string.debug_oldestLoadedComicReachedOnSwipe), Toast.LENGTH_SHORT).show()
+                                } else {
+                                    replaceFragment(comic.id - 1)
+                                }
                             } else {
-                                replaceFragment(comic.id - 1)
+                                Toast.makeText(context, getString(R.string.oldestComicReachedOnSwipe), Toast.LENGTH_SHORT).show()
                             }
-                        } else {
-                            Toast.makeText(context, getString(R.string.oldestComicReachedOnSwipe), Toast.LENGTH_SHORT).show()
                         }
-                    }
 
-                    override fun onSwipeLeft() {
-                        Log.d(LOG_TAG, "Detected left swipe")
-                        if (!comic.isLatest) {
-                            replaceFragment(comic.id + 1)
-                        } else {
-                            Toast.makeText(context, getString(R.string.latestComicReachedOnSwipe), Toast.LENGTH_SHORT).show()
+                        override fun onSwipeLeft() {
+                            Log.d(LOG_TAG, "Detected left swipe")
+                            if (!comic.isLatest) {
+                                replaceFragment(comic.id + 1)
+                            } else {
+                                Toast.makeText(context, getString(R.string.latestComicReachedOnSwipe), Toast.LENGTH_SHORT).show()
+                            }
                         }
-                    }
-                })
+                    })
+                }
             }
         }
+
 
         return rootView
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        if (binder != null) {
-            context?.unbindService(serviceConnection)
-        }
+        Picasso.get().cancelTag(LOG_TAG)
     }
 
     private fun replaceFragment(comicId: Int) {
